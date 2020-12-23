7865bfac4ad2a41fa16af34d77185e8dac891666
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2020-12-23 11:24:56 -0800

    initial demo project

---
+++ src/main/java/com/example/sleuthgh1729/SleuthGh1729Application.java
@@ -0,0 +1,13 @@
+package com.example.sleuthgh1729;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleuthGh1729Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleuthGh1729Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sleuthgh1729/SleuthGh1729ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sleuthgh1729;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleuthGh1729ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
